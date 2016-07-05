94ee4243c3314229d87056ed66dccac195b770f4
Kyu Taek Hwang <kyutaekhwang@gmail.com>
2016-07-05 19:36:31 -0400

    updated homework

--- javascript.js
+++ javascript.js
@@ -39,11 +39,71 @@ var favorite_food = ["Noodles", "Chicken"];
 
 var profile = [full_name, favorite_food];
 
-//debugging on console, arrayception
+// debugging on console, arrayception
 console.log(full_name[0], favorite_food[1]);
 
 // data type array, keys definition
 var full_name = {first: "Kyu", last: "Hwang"};
 
-//debugging on console
+// debugging on console
 console.log(full_name.last);
+
+// testing
+"stringone" === "stringtwo" //false
+"stringone" !== "stringtwo" //true
+5 < 10 //true
+5 > 10 //false
+
+// if test
+if(5>10) {
+	console.log("You'll never see this message");
+} else {
+	console.log("You'll always see this message");
+}
+
+// function example 1
+function someName(numberOne, otherNumber){
+	return numberOne + 10 + otherNumber;
+}
+
+console.log(someName(4,14)); //28
+
+// function example 2
+function alertName(somePersonsName) {
+	return alert(somePersonsName);
+}
+
+alertName("Zach");
+
+// loops-for example 1
+var friendNames = ["Kyu", "Eunice", "Bentley"];
+
+for(var i = 0; i <friendNames.length; i++) {
+	console.log(friendNames[i] + " is my friend")
+}
+
+// loops-for example 2
+for(var i = 0; i<10; i++){
+	console.log(i);
+}
+
+// loops-while example 1
+var num = 0;
+while(num < 11) {
+	console.log(num);
+	num++;
+}
+
+// loops-while example 2
+var num = 99;
+while(num >= 0) {
+	var unit;
+
+	if (num === 1) {
+		unit = " bottle "
+	} else {
+		unit = " bottles "
+	}
+	console.log(num + unit + "of root beer on the wall " + num + unit + "of root beer... take one down, pass it around");
+	num--;
+} 
